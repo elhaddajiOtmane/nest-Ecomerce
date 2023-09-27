569bb99f8edd091f6c23e756187ecfcf84d4ecfd
elhaddaji otmane <otmanhaddaji15@gmail.com>
2023-09-27 11:00:01 +0100

    remove space

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev 
-    ? "http://localhost:3000" 
+export const server = dev
+    ? "http://localhost:3000"
     : "https://nest-nextjs.vercel.app";
